c22ff1258cd10ea1e6351430ba26a0f5f6a4ee21
Timothy Dai <timothydai@DN525jil.SUNet>
2022-11-01 22:50:11 -0700

    Debug form

--- send_post.php
+++ send_post.php
@@ -2,13 +2,13 @@
     // Connecting to sql db.
     $connect = mysqli_connect("db4free.net", "timdai", "172cb102", "birdnet_ids");
 
-    $query_format = "INSERT INTO expert_ids VALUES (%d, %d, %s, %s, %s)"
-    // Sending form data to sql db.
-    mysqli_query(
-        $connect,
-        sprintf(
-            $query_format,
-            1, 1, $_POST["species_common_name"], $_POST["comments"], date("Y-m-d H:i:s")
-        )
-    )
+    // $query_format = "INSERT INTO expert_ids VALUES (%d, %d, %s, %s, %s)"
+    // // Sending form data to sql db.
+    // mysqli_query(
+    //     $connect,
+    //     sprintf(
+    //         $query_format,
+    //         1, 1, $_POST["species_common_name"], $_POST["comments"], date("Y-m-d H:i:s")
+    //     )
+    // )
 ?>
