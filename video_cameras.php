be70b84988cae561d1f8a5465d43764e42111b62
Timothy Dai <timothydai@DN0a2226f4.SUNet>
2022-11-02 20:27:59 -0700

    Add other locations

---
+++ video_cameras.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Labeler - Bat Monitoring Recorders</title>
+  </head>
+  <body>
+    <div>Bat recordings</div>
+    <div>Choose a location:</div>
+    <div>
+      <a href="labeler.php?location=Cam01&sample=0">Cam01</a>
+      | 
+      <a href="labeler.php?location=Cam02&sample=0">Cam02</a>
+      | 
+      <a href="labeler.php?location=Cam04&sample=0">Cam04</a>
+      | 
+      <a href="labeler.php?location=Cam07&sample=0">Cam07</a>
+      | 
+      <a href="labeler.php?location=Cam09&sample=0">Cam09</a>
+      | 
+      <a href="labeler.php?location=Cam10&sample=0">Cam10</a>
+    </div>
+    <img src="static_images/video_camera_map.png" width="800">
+    <div>
+      <a href="index.html">Back to Labeler Home</a>
+    </div>
+  </body>
+</html>
